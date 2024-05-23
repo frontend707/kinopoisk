2dc2d1a084ff81af2d36eefe393cb304c9129f22
Frontend <shomilisayevich707@gmail.com>
2024-05-23 14:33:44 +0500

    fix js step2

--- js/main.js
+++ js/main.js
@@ -2,8 +2,135 @@
 
 document.getElementById('themeChange').addEventListener ('click', changeTheme)
 
+let loader = document.querySelector('.loader')
+
 function changeTheme(){
     let body = document.querySelector('body')
     body.classList.toggle('dark')
 
+}
+
+
+async function sendRequest(url, method, data) {
+    if (method == "POST") {
+        let response = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+    } else if(method == "GET") {
+        url = url + "?" + new URLSearchParams(data)
+        let response = await fetch(url, {
+            method: "GET",
+        })
+        response = await response.json()
+        return response
+    }
+}
+
+
+
+document.getElementById('serarchBtn').addEventListener('click', findMovie)
+
+async function findMovie() {
+    let title = document.getElementById('search').value
+    // console.log(title)
+
+    loader.style.display = "block"
+
+    let movie = await sendRequest("http://www.omdbapi.com/", "GET", {
+        "apikey": "a5cda678",
+        "t": title
+    })
+    // console.log(movie)
+    if (movie.Response == "False") {
+        alert(movie.Error)
+    } else {
+        loader.style.display = "none"
+        showMovie(movie)
+        findSimilarmovies(title)
+        document.getElementById('film').style.display = "block"
+    }
+
+
+}
+
+
+function showMovie(movie){
+    document.querySelector("#film h2").innerHTML = movie.Title
+    // console.log(document.querySelector(".movieCover"))
+    document.querySelector(".movieCover").style.backgroundImage = `url(${movie.Poster})`
+
+    let array = ["Year", "Released","Runtime", "Genre", "Writer", "Actors", "Language", "Country", "imdbRating", "Type", "totalSeasons"]
+
+    let movieInfos = document.querySelector('.movieInfos')
+    movieInfos.innerHTML = ""
+
+    for(let i = 0; i < array.length; i++) {
+        let key = array[i]
+        let line = `<div class="movieInfo bg-darkest">
+            <div class="title">
+                ${key}
+            </div>
+            <div class="value">
+                ${movie[key]}
+            </div>
+        </div>`
+
+        movieInfos.innerHTML = movieInfos.innerHTML + line
+
+
+    }
+
+
+}
+
+
+async function findSimilarmovies(title) {
+  
+    let similar = await sendRequest("http://www.omdbapi.com/", "GET", {
+        "apikey": "a5cda678",
+        "s": title
+    })
+    // console.log(movie)
+    if (similar.Response == "False") {
+        document.querySelector('#similarFilms h2').innerHTML = `Poxoji filmov 0`
+        let similarMovies = document.querySelector('.similarMovies')
+        similarMovies.innerHTML = ""
+        // alert(smilar.Error)
+    } else {
+        
+        showSimilarMovie(similar.Search)
+        document.getElementById('similarFilms').style.display = "block"
+    }
+}
+
+function showSimilarMovie(movies) {
+    let similarMovies = document.querySelector('.similarMovies')
+    similarMovies.innerHTML = ""
+    j = 0
+    for(let i = 0; i < movies.length; i++) {
+        let movie = movies[i]
+        if (movie.Poster != 'N/A') {
+            j++
+            let movieDiv = `    
+            <div class="similarMovie" style="background-image: url('${movie.Poster}');">
+                        <div class="fav bg-dark">
+                            <img src="./img/favBtn.svg" alt="favorites">
+                        </div>
+                        <div class="similarTitile">
+                            ${movie.Title}
+                        </div>
+                    </div>
+            `
+            similarMovies.innerHTML = similarMovies.innerHTML + movieDiv
+
+        }
+        
+
+    }
+    document.querySelector('#similarFilms h2').innerHTML = `Poxoji filmov ${j}`
 }
